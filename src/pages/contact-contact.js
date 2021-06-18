f9ebac428679d92a05faaa5ef0e5a10a75c71e4b
AshotSimonyan <a.simonyan.stdev@gmail.com>
2021-06-18 12:23:14 +0400

    test

---
+++ src/pages/contact-contact.js
@@ -0,0 +1,22 @@
+import React from "react"
+import Layout from "../components/layout"
+const ContactUs = () => {
+
+  return (
+    <Layout>
+      <form name="contact" netlify >
+        <p>
+          <label>Name <input type="text" name="name" /></label>
+        </p>
+        <p>
+          <label>Email <input type="email" name="email" /></label>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
+      </form>
+    </Layout>
+  )
+}
+
+export default ContactUs

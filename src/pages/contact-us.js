d0feca082044f4cabc9288565f97bf24f330e304
AshotSimonyan <a.simonyan.stdev@gmail.com>
2021-06-23 11:51:33 +0400

    contact us view

--- src/pages/contact-us.js
+++ src/pages/contact-us.js
@@ -33,14 +33,16 @@ const ContactUs = () => {
     contactList,
   } = frontmatter
 
+  console.log({ data })
+
   return (
     <Layout>
       <section className="small-hero">
         <div className="container">
           <div className="small-hero-content">
             <div className="small-hero-text">
-              <h1 className="h1">Catchy title for contact us page goes here</h1>
-              <p>When it comes to pets, let us do the heavy lifting for as little as $5/month</p>
+              <h1 className="h1">{heroHeading}</h1>
+              <p>{heroText}</p>
             </div>
           </div>
         </div>
@@ -50,49 +52,26 @@ const ContactUs = () => {
         <div className="container">
           <div className="contact-content">
             <div className='contact-list-wrapper'>
-              <h2 className='h2'>Reach us here</h2>
+              <h2 className='h2'>{sec1Heading}</h2>
               <ul className='contact-list'>
-                <li>
-                  <Icon
-                    icon='phone'
-                    size={24}
-                  />
-                  (305) 555-4446
-                </li>
-                <li>
-                  <Icon
-                    icon='cellphone-android'
-                    size={24}
-                  />
-                  (305) 555-4555
-                </li>
-                <li>
-                  <Icon
-                    icon='email-outline'
-                    size={24}
-                  />
-                  wpestate@gmail.com
-                </li>
-                <li>
-                  <Icon
-                    icon='fax'
-                    size={24}
-                  />
-                  (305) 555-4555
-                </li>
-                <li>
-                  <Icon
-                    icon='skype'
-                    size={24}
-                  />
-                  skypeidname
-                </li>
+                {
+                  contactList.map(({contactField, icon}, index) => {
+                    return (
+                      <li key={index}>
+                        <Icon
+                          icon={icon}
+                          size={24}
+                        />
+                        {contactField}
+                      </li>
+                    )
+                  })
+                }
               </ul>
             </div>
             <ContactForm />
           </div>
         </div>
-
       </section>
 
 

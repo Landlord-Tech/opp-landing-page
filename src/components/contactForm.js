cc1bf51d282336425cb92e463129d963c155da0d
AshotSimonyan <a.simonyan.stdev@gmail.com>
2021-06-28 22:22:37 +0400

    small fix

--- src/components/contactForm.js
+++ src/components/contactForm.js
@@ -38,7 +38,7 @@ const ContactForm = () => {
     fetch("/", {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({ "form-name": "contact", ...formState })
+      body: encode({ "form-name": "contact-us", ...formState })
     })
       .then(() => alert("Success!"))
       .catch(error => alert(error));
